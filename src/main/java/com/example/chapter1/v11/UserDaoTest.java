571c6fa6c04930d517269c6ccf8d4d0192af3d0c
nonaninona <starcraft0529@gmail.com>
2025-02-07 22:48:07 +0900

    refactor : 1.8 xml - v11 ConnectionMaker를 Datasource로 변환, XML 프로퍼티 주입

--- src/main/java/com/example/chapter1/v11/UserDaoTest.java
+++ src/main/java/com/example/chapter1/v11/UserDaoTest.java
@@ -1,14 +1,15 @@
-package com.example.chapter1.v10;
+package com.example.chapter1.v11;
 
 import java.sql.SQLException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("chapter1/v9/applicationContext.xml");
+//        ApplicationContext context = new GenericXmlApplicationContext("chapter1/v11/applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao1 = context.getBean("userDao", UserDao.class);
-        CountingConnectionMaker ccm = context.getBean("countingConnectionMaker", CountingConnectionMaker.class);
 
         User user = new User();
         user.setId("ihh0529");
@@ -22,7 +23,5 @@ public class UserDaoTest {
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
         System.out.println(user2.getId() + " 조회 성공");
-
-        System.out.println("count = " + ccm.getCount());
     }
 }
