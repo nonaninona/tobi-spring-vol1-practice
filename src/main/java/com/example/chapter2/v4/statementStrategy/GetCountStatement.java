deffb5085bb4925656f17aba9a54ba30a1470bd8
nonaninona <starcraft0529@gmail.com>
2025-02-12 16:01:29 +0900

    feat : 3.2 변하는 것과 변하지 않는 것 - v4 DI 적용

---
+++ src/main/java/com/example/chapter2/v4/statementStrategy/GetCountStatement.java
@@ -0,0 +1,14 @@
+package com.example.chapter2.v4.statementStrategy;
+
+import com.example.chapter2.v3.UserDao;
+import com.example.chapter2.v4.StatementStrategy;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GetCountStatement implements StatementStrategy {
+    @Override
+    public PreparedStatement makeStatement(Connection c) throws SQLException {
+        return c.prepareStatement("select count(*) from users");
+    }
+}
